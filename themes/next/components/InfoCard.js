0132e3dbf8e85bb35bbafead11170a0d5917cff1
MuLiChFe <76549977+MuLiChFe@users.noreply.github.com>
2022-09-23 15:23:50 +1000

    Update InfoCard.js

--- themes/next/components/InfoCard.js
+++ themes/next/components/InfoCard.js
@@ -7,8 +7,8 @@ const InfoCard = (props) => {
   const { siteInfo } = props
   return <>
     <div className='flex flex-col items-center justify-center '>
-        <div className='text-4xl font-serif dark:text-white py-2 pl-4 hover:scale-105 transform duration-200'>{BLOG.AUTHOR}</div>
-        <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200'>{BLOG.BIO}</div>
+        <div className='text-4xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{BLOG.AUTHOR}</div>
+        <div className='font-light dark:text-white py-2 text-sm hover:scale-105 transform duration-200'>{BLOG.BIO}</div>
         <SocialButton/>
     </div>
   </>
