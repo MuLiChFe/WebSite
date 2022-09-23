1b81625e35f45ec13800efcfb318af6dabce0906
MuLiChFe <76549977+MuLiChFe@users.noreply.github.com>
2022-09-23 14:49:32 +1000

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
                                                                         // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: 'TGCS', // 作者
-  BIO: 'TGC Survival', // 作者简介
+  AUTHOR: 'TGC Survival', // 作者
+  BIO: 'TGCSurvival.minehut.gg', // 作者简介
   LINK: 'https://tgcs.vercel.app/', // 网站地址
   KEYWORDS: '', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID: '09710b8e449d41979875baa6818977f4', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
